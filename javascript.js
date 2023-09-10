0a222534324ca830da91cb1f5d2deb3c8e675fac
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-09-10 23:30:29 +1000

    Added playing a round function and player choice prompt

--- javascript.js
+++ javascript.js
@@ -1,18 +1,34 @@
-let Scissors = 0;
-let Paper = 1;
-let Rock = 2;
+let scissors = 0;
+let paper = 1;
+let rock = 2;
+let playerScore = null;
+let computerScore = null;
 
-
-function getComputerChoice() {
-    result = (Math.floor(Math.random() * 3));
-    if (result == 0) {
+function getComputersChoice() {
+    numericalResult = (Math.floor(Math.random() * 3));
+    if (numericalResult == 0) {
         console.log("The computer has chosen Scissors")
+        result = "scissors";
     }
-    else if (result == 1) {
+    else if (numericalResult == 1) {
         console.log("The computer has chosen Paper.")
+        result = "paper";
     }
     else {
         console.log("The computer has chosen Rock.")
+        result = "rock";
     }
     return result;
+}
+
+function getPlayersChoice() {
+    playersChoice = prompt("Please choose scissors, paper or rock.");
+    return playersChoice.toLowerCase()
+}
+
+function playARound() {
+    computersChoice = getComputersChoice();
+    playersChoice = getPlayersChoice();
+    console.log(computersChoice);
+    console.log(playersChoice);
 }
