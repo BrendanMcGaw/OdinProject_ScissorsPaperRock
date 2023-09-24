1099a2e7fb3c8d4cd0d1afa9252f86b031c84ca4
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-09-24 19:27:25 +1000

    Added functionality for score keeping and first to 5 wins the game alert

--- javascript.js
+++ javascript.js
@@ -32,23 +32,20 @@ document.getElementById("Paper").addEventListener("click", () => {
 document.getElementById("Rock").addEventListener("click", () => {
     playARound("rock")
 })
-// document.getElementById("Paper").addEventListener("click", getPlayersChoice());
-// document.getElementById("Rock").addEventListener("click", getPlayersChoice());
-// console.log(getPlayersChoice);
-
-// function getPlayersChoice() {
-//     return playersChoice.toLowerCase()
-// }
 
 // This function plays a round of the game and determines a victor.
 function playARound(playersChoice) {
+
+    console.log("The final score was:");
+    console.log(playerScore + " scored by the player.");
+    console.log(computerScore + " scored by the computer.");
+
     computersChoice = getComputersChoice();
-    console.log(computersChoice);
-    console.log(playersChoice);
 // These 3 if statements declare the player the winner and give the player a point if the conditions are met.
     if (playersChoice == "scissors" && computersChoice == "paper") {
         document.getElementById("match-result").innerHTML = "The player has won this round!";
         ++playerScore;
+        document.getElementById("running-score").innerHTML = "The <br> running <br> score"
     }
     else if (playersChoice == "paper" && computersChoice == "rock") {
         document.getElementById("match-result").innerHTML = "The player has won this round!";
@@ -75,24 +72,18 @@ function playARound(playersChoice) {
     else {
         document.getElementById("match-result").innerHTML = "This round was a draw.";
     }
+
+    document.getElementById("running-score").innerHTML = 
+    "Human: " +  playerScore +
+    "<br> Computer: " + computerScore;
+
+    if (playerScore == 5) {
+        alert("The player has won the game! Congratulations");
+        return;
+    }
+
+    else if (computerScore == 5) {
+        alert("The computer has risen up and stolen victory! Commiserations!");
+        return;
+    }
 }
-// function game() {
-//     // This is not working as intended, computer score at 5 or above is not stopping the run.
-//     // it would appear that its waiting until both numbers are ABOVE 5 before it stops running
-//     while (playerScore < 5 && computerScore < 5) {
-//         playARound();
-//         console.log(playerScore);
-//         console.log(computerScore);
-//     }
-
-//     if (playerScore == 5) {
-//         alert("The player has won the game! Congratulations");
-//     }
-
-//     else if (computerScore == 5) {
-//         alert("The computer has risen up and stolen victory! Commiserations!");
-//     }
-//     console.log("The final score was:");
-//     console.log(playerScore + " scored by the player.");
-//     console.log(computerScore + " scored by the computer.");
-// }
