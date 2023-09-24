518d41f6d8acf1d4b2bb34c0dd6ced6df167655a
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-09-24 14:02:23 +1000

    Added innerhtml changes to follow who is winning each round

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ let rock = 2;
 let playersChoice = null;
 let playerScore = null;
 let computerScore = null;
+let results = document.querySelector('p.results');
 
 function getComputersChoice() {
     numericalResult = (Math.floor(Math.random() * 3));
@@ -46,33 +47,33 @@ function playARound(playersChoice) {
     console.log(playersChoice);
 // These 3 if statements declare the player the winner and give the player a point if the conditions are met.
     if (playersChoice == "scissors" && computersChoice == "paper") {
-        console.log("The player has won this round!");
+        document.getElementById("match-result").innerHTML = "The player has won this round!";
         ++playerScore;
     }
     else if (playersChoice == "paper" && computersChoice == "rock") {
-        console.log("The player has won this round!");
+        document.getElementById("match-result").innerHTML = "The player has won this round!";
         ++playerScore;
     }
     else if (playersChoice == "rock" && computersChoice == "scissors") {
-        console.log("The player has won this round!");
+        document.getElementById("match-result").innerHTML = "The player has won this round!";
         ++playerScore;
     }
 // These 3 if statements declare the computer the winner and give the computer a point if the conditions are met.
     else if (playersChoice == "scissors" && computersChoice == "rock") {
-        console.log("The computer has won this round!");
+        document.getElementById("match-result").innerHTML = "The computer has won this round!";
         ++computerScore;
     }
     else if (playersChoice == "paper" && computersChoice == "scissors") {
-        console.log("The computer has won this round!");
+        document.getElementById("match-result").innerHTML = "The computer has won this round!";
         ++computerScore;
     }
     else if (playersChoice == "rock" && computersChoice == "paper") {
-        console.log("The computer has won this round!");
+        document.getElementById("match-result").innerHTML = "The computer has won this round!";
         ++computerScore;
     }
 // This else declares the round was a draw.
     else {
-        console.log("This round was a draw. No points awarded.");
+        document.getElementById("match-result").innerHTML = "This round was a draw.";
     }
 }
 // function game() {
